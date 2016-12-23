743b3608f8ac65e5f062fc4b6ea167492169bfc8
Leandro Emiliano Guimarães | Stone <lguimaraes@stone.com.br>
2016-12-23 15:06:07 -0200

    :construction: Adicionado projetos, servidor e cliente.

---
+++ WhereTheMagicHappns/WhereTheMagicHappns/Models/Conta.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WhereTheMagicHappns.Models
+{
+    public class Conta
+    {
+        public string Agencia { get; set; }
+        public string NumConta { get; set; }
+        public string Senha { get; set; }
+    }
+}
